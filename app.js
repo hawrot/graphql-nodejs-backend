ae1eea03382563fc49dbe4f092e687e3ed353b11
hawrot <mhawro200@caledonian.ac.uk>
2020-02-05 16:00:45 +0000

    Fetch single post

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-
 const graphqlHttp = require('express-graphql');
+
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
-
-
-
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -25,9 +24,9 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
   ) {
     cb(null, true);
   } else {
@@ -38,40 +37,57 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if(req.method === 'OPTIONS'){
+  if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
   next();
 });
-const auth = require('./middleware/auth');
+
 app.use(auth);
 
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true,
-  formatError(err){
-    if(!err.originalError){
-      return err;
-    }
-    const data = err.originalError.data;
-    const message = err.message || 'An error occurred';
-    const code = err.originalError.code || 500;
-    return {message :message, status: code, data: data};
-
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
   }
-}));
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+      .status(201)
+      .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+app.use(
+    '/graphql',
+    graphqlHttp({
+      schema: graphqlSchema,
+      rootValue: graphqlResolver,
+      graphiql: true,
+      formatError(err) {
+        if (!err.originalError) {
+          return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occurred.';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+      }
+    })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -80,6 +96,7 @@ app.use((error, req, res, next) => {
   const data = error.data;
   res.status(status).json({ message: message, data: data });
 });
+
 
 mongoose
   .connect(
@@ -90,3 +107,8 @@ app.listen(8080);
 
   })
   .catch(err => console.log(err));
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+};
